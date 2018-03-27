c1a71a4c028bf9b40d8e8e3b46c904009d1894ab
Kamran  Kowsari <kk7nc@virginia.edu>
2018-03-27 14:15:56 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='HDLTex',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/kk7nc/HDLTex',
+    license='MIT',
+    author='Kamran Kowsari',
+    author_email='kk7nc@virginia.edu',
+    description='HDLTex: Hierarchical Deep Learning for Text Classification',
+    long_description=read('README.md'),
+    install_requires=required
+)
