c44acf54d01b520fe4b4726224b74e1203354edd
Kamran  Kowsari <kk7nc@virginia.edu>
2018-04-16 18:32:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,53 @@
-from setuptools import setup
+import io
 import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+import numpy
+from setuptools import Extension
+from setuptools import setup, find_packages
+
+__author__ = 'Kamran Kowsari <kowsari.net>'
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def readfile(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    return io.open(path, encoding='utf8').read()
+
 
 setup(
     name='HDLTex',
-    version='1.0.1',
+    version='1.0.2',
+    description='HDLTex: Hierarchical Deep Learning for Text Classification',
+    long_description=readfile('README.md'),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: Linguistic',
+    ],
     url='https://github.com/kk7nc/HDLTex',
-    license='MIT',
     author='Kamran Kowsari',
     author_email='kk7nc@virginia.edu',
-    description='HDLTex: Hierarchical Deep Learning for Text Classification',
-    long_description=read('README.md'),
-    install_requires=required
+    packages=['HDLTex'] + find_packages('HDLTex'),
+    install_requires=[
+        'matplotlib==2.1.2',
+        'numpy==1.12.1',
+        'pandas==0.22.0',
+        'scipy',
+        'tensorflow-gpu',
+        'keras==2.0.9',
+        'scikit-learn==0.19.0',
+        'nltk==3.2.4'
+    ],
+    include_package_data=False
 )
